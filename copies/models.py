463d7a32ea370e599b4ab3a275b9ea3219f52b7b
unknown <lucasribeiro0308@gmail.com>
2023-06-30 14:03:10 -0300

    feat:app copies

---
+++ copies/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Copy(models.Model):
+    in_stock = models.BooleanField(default=True, null=True)
+    book = models.ForeignKey(
+        "books.Book",
+        on_delete=models.CASCADE,
+        related_name="copies"
+    )
---
+++ copies/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Copy
+
+class CopySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Copy
+        fields = ["id","in_stock","book_id"]
+        extra_kwargs = {"book_id":{"read_only":True}}
