6f53b4cabd5ea69e0a509a38ddd6cfe5c0899e0b
toledomg <toledomg@yahoo.com.br>
2023-06-30 09:58:18 -0300

    :rocket: Teste de Deploy no RailRaw

--- _core/settings.py
+++ _core/settings.py
@@ -159,7 +159,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 5,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_FILTER_BACKENDS": "django_filters.res_framework.DjangoFilterBackend",
 }
 
 SPECTACULAR_SETTINGS = {
