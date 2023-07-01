3dbfc452ade4df1bc4b00bf19c61094faa9a1a6a
toledomg <toledomg@yahoo.com.br>
2023-07-01 10:59:04 -0300

    Deploy Railraw

--- _core/settings.py
+++ _core/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", False)
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
