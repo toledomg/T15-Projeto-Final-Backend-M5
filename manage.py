7e38b35a1a9dd1966426ee4a2458791c7e3adec3
toledomg <toledomg@yahoo.com.br>
2023-07-01 10:32:00 -0300

    Deploy Railraw

--- manage.py
+++ manage.py
@@ -2,19 +2,19 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.core.management.commands.runserver import Command as runserver
+# from django.core.management.commands.runserver import Command as runserver
 
-import dotenv
+# import dotenv
 
-dotenv.load_dotenv(dotenv.find_dotenv())
+# dotenv.load_dotenv(dotenv.find_dotenv())
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_core.settings")
 
-    # Alterar Porta default do Servidor Django `runserver` command
-    runserver.default_port = os.getenv("DJ_SERVER")
+    # # Alterar Porta default do Servidor Django `runserver` command
+    # runserver.default_port = os.getenv("DJ_SERVER")
 
     try:
         from django.core.management import execute_from_command_line
@@ -30,4 +30,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+# dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
