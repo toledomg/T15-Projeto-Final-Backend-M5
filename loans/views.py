04090b1cfa03961980966cf8a269bf673087c546
toledomg <toledomg@yahoo.com.br>
2023-07-03 14:53:32 -0300

    recycle: refactor: adicionado proteções e permissões na loans

--- loans/views.py
+++ loans/views.py
@@ -1,14 +1,23 @@
-from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from .models import Loans
 from .serializers import LoansSerializer
+from users.permissions import IsLibraryCollaboratorOrOwner
 
 
-class LoanView(generics.ListCreateAPIView):
+class LoanView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsLibraryCollaboratorOrOwner]
+
     queryset = Loans.objects.filter(is_returned=False)
     serializer_class = LoansSerializer
 
 
-class LoanDetailView(generics.RetrieveUpdateDestroyAPIView):
+class LoanDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsLibraryCollaboratorOrOwner]
+
     queryset = Loans.objects.all()
     serializer_class = LoansSerializer
 
