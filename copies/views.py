fba5cae091be07241fb91262f23282fc973fe6b2
unknown <lucasribeiro0308@gmail.com>
2023-07-03 13:56:28 -0300

    fix:copies functions

--- copies/views.py
+++ copies/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Copy
+from .serializers import CopySerializer
+from rest_framework import generics
+from books.models import Book
 
-# Create your views here.
+class CopyBookView(generics.ListAPIView):
+    serializer_class = [CopySerializer]
+
+    def get_queryset(self):
+       queryset=super().get_queryset()
+       instance_book = get_object_or_404(Book, pk=self.kwargs.get("pk"))
+
+       return queryset.filter(book=instance_book)
