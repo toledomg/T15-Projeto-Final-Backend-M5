afdef79f256cfad4eb0f32f9aeb71e22f0f1cdbf
Laisa <laisacristina2606@gmail.com>
2023-07-04 14:27:03 -0300

    feat: update loans

--- loans/models.py
+++ loans/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-from datetime import timedelta
 from django.utils import timezone
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class Loans(models.Model):
@@ -21,46 +18,5 @@ class Loans(models.Model):
     copy = models.ForeignKey(
         "copies.Copy",
         on_delete=models.PROTECT,
-        related_name="copy"
+        related_name="loans"
     )
-
-    def save(self, *args, **kwargs):
-        if not self.loan_return:
-            self.loan_return = self.loan_initial + timedelta(days=5)
-            if self.loan_return.weekday() in [5, 6]:
-                self.loan_return += timedelta(
-                    days=5 - self.loan_return.weekday())
-
-        if self.loan_returne and self.loan_returne < timezone.now():
-            self.is_delay = True
-            self.blocking_date = timezone.now() + timedelta(days=7)
-
-        if self.is_returned:
-            self.blocking_date = timezone.now() + timedelta(days=7)
-            self.is_delay = True
-
-        super().save(*args, **kwargs)
-
-        self.user.update_blocked_status()
-
-
-@receiver(post_save, sender=Loans)
-def update_user_blocked_status(sender, instance, **kwargs):
-    instance.user.update_blocked_status()
-
-#  Alexsandro aqui estara chamando a função para fazer o updated do is_active
-# na tabela do usuario
-#       a função precisa ser algo assim:
-#
-#       class User(AbstractUser):
-#           is_active = models.BooleanField(default=False)
-
-#     def update_blocked_status(self): (exemplo de nome da função)
-#         delay_loan_books = self.Loans_set.filter(is_delay=True)
-
-#         if delay_loan_books.exists():
-#             self.is_active = False
-#         else:
-#             self.is_active = True
-
-#         self.save()
--- loans/serializers.py
+++ loans/serializers.py
@@ -1,6 +1,9 @@
-from .models import Loans
 from rest_framework import serializers
 from loans.models import Loans
+from django.db.models.signals import post_save
+from datetime import timedelta
+from django.utils import timezone
+from django.dispatch import receiver
 
 
 class LoansSerializer(serializers.ModelSerializer):
@@ -13,7 +16,39 @@ class LoansSerializer(serializers.ModelSerializer):
         return loan
 
     def update(self, instance, validated_data):
-        instance.return_date = validated_data.get(
+        instance.loan_return = validated_data.get(
             'loan_return', instance.loan_return)
         instance.save()
         return instance
+
+    def save(self, *args, **kwargs):
+        if not self.instance.loan_return:
+            self.instance.loan_return = self.instance.loan_initial + timedelta(
+                days=5)
+
+            if self.instance.loan_return.weekday() in [5, 6]:
+                self.instance.loan_return += timedelta(
+                    days=5 - self.instance.loan_return.weekday())
+
+        if self.instance.loan_return <= timezone.now():
+            self.instance.is_delay = False
+            # self.instance.blocking_date = timezone.now() + timedelta(days=7)
+
+        if self.instance.loan_return > timezone.now():
+            self.instance.blocking_date = timezone.now() + timedelta(days=7)
+            self.instance.is_delay = True
+
+        return super().save(*args, **kwargs)
+
+    def update_blocked_status(self):
+        delay_loan_books = self.user.loans.filter(is_delay=True)
+        if delay_loan_books.exists():
+            self.is_active = False
+        else:
+            self.is_active = True
+        self.user.update_blocked_status()
+
+
+@receiver(post_save, sender=Loans)
+def update_user_blocked_status(sender, instance, **kwargs):
+    instance.user.update_blocked_status()
