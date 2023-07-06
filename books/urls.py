f8456177f962ada4c556d7b3dc6c98d63616da2f
unknown <lucasribeiro0308@gmail.com>
2023-07-06 14:49:27 -0300

    feat:Follow create/get

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import BookView, BookDetailView
+from .views import BookView, BookDetailView, FollowCreateView,FollowedBooksListView
 
 urlpatterns = [
     path("books/", BookView.as_view()),
     path("books/<int:pk>/", BookDetailView.as_view()),
+    path("books/follow/", FollowCreateView.as_view()),
+    path("books/followed-books/", FollowedBooksListView.as_view())
+
 ]
--- books/views.py
+++ books/views.py
@@ -1,10 +1,11 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView,CreateAPIView, ListAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from users.permissions import IsAdminOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
-from .models import Book
-from .serializers import BookSerializer
+from .models import Book, Follow
+from .serializers import BookSerializer, FollowSerializer, FollowedBooksSerializer
 
 
 class BookView(ListCreateAPIView):
@@ -22,3 +23,22 @@ class BookDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = "pk"
+
+class FollowCreateView(CreateAPIView):
+    serializer_class = FollowSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        book_title = self.request.data.get('title')
+        book = Book.objects.get(title=book_title)
+        serializer.save(user=self.request.user, book=book)
+
+class FollowedBooksListView(ListAPIView):
+    serializer_class = FollowedBooksSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Follow.objects.filter(user=user)
