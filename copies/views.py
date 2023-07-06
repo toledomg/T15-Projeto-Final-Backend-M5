3112ba030bad525a65bd85fe0e3fe052460e19ee
Laisa <laisacristina2606@gmail.com>
2023-07-06 11:50:10 -0300

    feat: updated loans

--- copies/views.py
+++ copies/views.py
@@ -17,7 +17,6 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 class CopyBookView(ListAPIView):
     serializer_class = CopySerializer
 
-
     def get_queryset(self):
         instance_book = get_object_or_404(Book, pk=self.kwargs.get("pk"))
         queryset = Copy.objects.filter(book=instance_book)
--- loans/views.py
+++ loans/views.py
@@ -8,17 +8,26 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Loans
 from .serializers import LoansSerializer
 from users.permissions import IsLibraryCollaboratorOrOwner
+from users.models import User
+from rest_framework.exceptions import PermissionDenied
 
 
 class LoanView(ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsLibraryCollaboratorOrOwner]
 
-
     queryset = Loans.objects.filter(is_returned=False)
     serializer_class = LoansSerializer
 
-    
+    def perform_create(self, serializer):
+        user = self.request.user
+        print(user)
+        if not user.is_allowed:
+            raise PermissionDenied("You are not allowed to borrow books.")
+        # user.is_allowed = False
+        # user.save()
+        # print(user)
+        serializer.save(user=user)
 
 
 class LoanDetailView(RetrieveUpdateDestroyAPIView):
