03693b51b311d4ef8bd4952069acf37f45e245bc
unknown <lucasribeiro0308@gmail.com>
2023-07-07 15:20:49 -0300

    feat:email to users

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BookView, BookDetailView, FollowCreateView, FollowedBooksListView
+from .views import BookView, BookDetailView, FollowCreateView, FollowedBooksListView,EmailNotificationView
 
 urlpatterns = [
     path("books/", BookView.as_view()),
     path("books/<int:pk>/", BookDetailView.as_view()),
     path("books/follow/", FollowCreateView.as_view()),
     path("books/followed-books/", FollowedBooksListView.as_view()),
+    path("send-email/", EmailNotificationView.as_view()),
 ]
