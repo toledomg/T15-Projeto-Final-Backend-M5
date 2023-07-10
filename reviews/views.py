0165f532c62b3ab17d71e1780655185e472748a2
Carol-Rocha <caroldsr1@gmail.com>
2023-07-10 19:03:37 -0300

    fix: check if the user rating already exists

--- reviews/views.py
+++ reviews/views.py
@@ -4,6 +4,8 @@ from .serializers import ReviewSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from users.permissions import IsLibraryCollaboratorOrOwner
+from books.models import Book
+from django.shortcuts import get_object_or_404
 
 
 class ReviewView(generics.ListCreateAPIView):
@@ -12,6 +14,16 @@ class ReviewView(generics.ListCreateAPIView):
 
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+
+class ReviewListView(generics.ListAPIView):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        instance_book = get_object_or_404(Book, pk=self.kwargs.get("pk"))
+        queryset = Review.objects.filter(book=instance_book)
+
+        return queryset
 
 
 class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
