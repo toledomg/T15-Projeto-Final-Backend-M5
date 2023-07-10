e8d70962250dbc41582eab30dccca35ee7c7642b
toledomg <toledomg@yahoo.com.br>
2023-07-10 12:22:27 -0300

    :bug: fix: corrigido a devolução de livros

--- loans/serializers.py
+++ loans/serializers.py
@@ -48,7 +48,7 @@ class LoansSerializer(serializers.ModelSerializer):
         copy = Copy.objects.get(id=copy_id)
 
         if not copy.is_available:
-            raise ValidationError("Não há copias disponíveis no momento")
+            raise ValidationError({"detail": "Não há copias disponíveis no momento"})
 
         copy.is_available = False
         copy.save()
@@ -58,44 +58,3 @@ class LoansSerializer(serializers.ModelSerializer):
         )
 
         return loan
-
-    # def update(self, instance, validated_data):
-
-    #     instance.loan_return = validated_data.get(
-    #         'loan_return', instance.loan_return)
-
-    #     instance.save()
-    #     return instance
-
-    # def save(self, *args, **kwargs):
-
-    #     if not self.instance.loan_return:
-    #         self.instance.loan_return = self.instance.loan_initial + timedelta(
-    #             days=5)
-
-    #         if self.instance.loan_return.weekday() in [5, 6]:
-    #             self.instance.loan_return += timedelta(
-    #                 days=5 - self.instance.loan_return.weekday())
-
-    #     if self.instance.loan_return <= timezone.now():
-    #         self.instance.is_delay = False
-    #         # self.instance.blocking_date = timezone.now() + timedelta(days=7)
-
-    #     if self.instance.loan_return > timezone.now():
-    #         self.instance.blocking_date = timezone.now() + timedelta(days=7)
-    #         self.instance.is_delay = True
-
-    #     return super().save(*args, **kwargs)
-
-    # def update_blocked_status(self):
-    #     delay_loan_books = self.user.loans.filter(is_delay=True)
-    #     if delay_loan_books.exists():
-    #         self.is_active = False
-    #     else:
-    #         self.is_active = True
-    #     self.user.update_blocked_status()
-
-
-# @receiver(post_save, sender=Loans)
-# def update_user_blocked_status(sender, instance, **kwargs):
-#   instance.user.update_blocked_status()
--- loans/views.py
+++ loans/views.py
@@ -1,17 +1,20 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import (
-    ListAPIView,
     ListCreateAPIView,
+    ListAPIView,
     RetrieveUpdateDestroyAPIView,
 )
+from rest_framework.views import Response, status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from datetime import datetime, timedelta
-from django.utils.timezone import make_aware
 
+from users.serializers import UserSerializer
 from .models import Loans
+from users.models import User
 from .serializers import LoansSerializer
 from users.permissions import IsLibraryCollaboratorOrOwner
-from users.models import User
 from rest_framework.exceptions import PermissionDenied, ValidationError
+from django.utils.timezone import make_aware, get_default_timezone
 
 
 class LoanView(ListCreateAPIView):
@@ -20,10 +23,13 @@ class LoanView(ListCreateAPIView):
 
     queryset = Loans.objects.filter(is_returned=False)
     serializer_class = LoansSerializer
+    lookup_url_kwarg = "pk"
 
     def perform_create(self, serializer):
         data_atual = datetime.now()
-        user = self.request.user
+        user_id = self.request.data.get("user_id")
+        user = User.objects.get(id=user_id)
+
         is_delay = Loans.objects.filter(user=user, is_delay=True)
 
         if is_delay:
@@ -43,8 +49,6 @@ class LoanView(ListCreateAPIView):
         if registros_atrasados.exists():
             raise ValidationError({"detail": "User with delay in returning loans"})
 
-        user = self.request.user
-
         if not user.is_allowed:
             raise PermissionDenied({"detail": "You are not allowed to borrow books."})
 
@@ -58,3 +62,55 @@ class LoanDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Loans.objects.all()
     serializer_class = LoansSerializer
     lookup_url_kwarg = "pk"
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if instance.is_returned:
+            return Response(
+                {"details": "Este livro já foi devolvido."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        current_datetime = datetime.now()
+        return_deadline = instance.loan_return.date()
+
+        if current_datetime.date() > return_deadline:
+            instance.blocking_date = current_datetime + timedelta(days=7)
+            instance.save()
+
+            instance.user.is_allowed = False
+            instance.user.save()
+
+            instance.loan_return = None
+            instance.copy.is_available = True
+            instance.copy.save()
+
+            return Response(
+                {"details": "A data limite de devolução foi ultrapassada."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        instance.loan_return = None
+        instance.copy.is_available = True
+        instance.copy.save()
+
+        instance.is_returned = True
+        instance.save()
+
+        serializer = self.get_serializer(instance)
+
+        return Response(serializer.data)
+
+
+class UserLoansDetailView(ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsLibraryCollaboratorOrOwner]
+
+    serializer_class = LoansSerializer
+
+    def get_queryset(self):
+        instance_user = self.request.user
+        queryset = Loans.objects.filter(user=instance_user)
+
+        return queryset
